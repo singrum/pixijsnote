c95e2eb9432d1638be4c054edfcbcef74f4be3ba
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-07-10 04:29:40 +0000

    x

--- note/note1_BZ/main.js
+++ note/note1_BZ/main.js
@@ -1,7 +1,101 @@
-import * as PIXI from '../../node_module/dist/pixi.min.js"';
+import PIXI from "../../node_modules/pixi.js/dist/pixi.js"
 
-// Create a PixiJS application of type cavas with specify background color and make it resizes to the iframe window
-const app = new PIXI.Application() < HTMLCanvasElement > { background: '#1099bb', resizeTo: window };
 
-// Adding the application's view to the DOM
-document.body.appendChild(app.view);
+
+
+
+
+
+class App{
+    constructor(){
+        const app = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight });
+        this.app = app;
+        document.body.appendChild(app.view);
+        this.setInteraction();
+        this.setTicker();
+
+    }
+    setInteraction(){
+        this.currPointers = [];
+        const downEvent = e => {
+            
+			if ('ontouchstart' in window) {
+                this.app.view.addEventListener("touchmove", moveEvent,false);
+                this.currPointers = e.targetTouches;
+                
+            }
+            else {
+                this.app.view.addEventListener("mousemove", moveEvent,false);
+                this.currPointers = [e];
+                
+            }
+		}
+		const moveEvent = e=>{
+			if ('ontouchstart' in window) {
+				this.currPointers = e.targetTouches
+
+			}
+			else {
+				this.currPointers = [e]
+
+			}
+		}
+		const upEvent = e => {
+			if ('ontouchstart' in window) {
+                this.app.view.removeEventListener("touchmove", moveEvent,false)
+                for (let touch of e.changedTouches) {
+					this.currPointers = this.currPointers.filter(e => e !== touch);
+				}
+            }
+            else {
+                this.app.view.removeEventListener("mousemove", moveEvent,false)
+                this.currPointers = []
+            }
+
+		}
+
+
+		if ('ontouchstart' in window) {
+            this.app.view.addEventListener('touchstart', downEvent);
+            this.app.view.addEventListener('touchend', upEvent);
+        }
+		else {
+            
+            this.app.view.addEventListener('mousedown', downEvent);
+            this.app.view.addEventListener('mouseup', upEvent);
+        }
+
+    }
+    setTicker(){
+        let counter = 0;
+        const period = 0.4;
+        const makeCircle = delta => {
+            counter += delta
+            if(counter > period){
+                counter = 0;
+
+                console.log(this.currPointers)
+
+            }
+        }
+        const growCircle = delta => {
+
+        }
+        const drawCircle = delta => {
+
+        }
+
+
+        this.app.ticker.add(delta=>{
+            makeCircle(delta);
+            growCircle(delta);
+            drawCircle(delta);
+            
+        })
+
+        
+    }
+
+}
+
+new App()
