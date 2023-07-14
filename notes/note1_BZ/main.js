9ab88e6ae0a74c743329ce531f77e1589a130e6c
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-07-14 12:56:57 +0900

    Update main.js

--- notes/note1_BZ/main.js
+++ notes/note1_BZ/main.js
@@ -408,7 +408,7 @@ class App {
 
         const renderTarget = PIXI.RenderTexture.create({ width: window.innerWidth, height: window.innerHeight }); 
         let sprite =  new PIXI.Sprite(renderTarget)
-        sprite.filters = [edgeFilter];
+        sprite.filters = [edgeFilter,new PIXI.FXAAFilter()];
         const applyFilter = () => {
             
             
