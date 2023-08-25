0cca069221ae5c2554a4fbd4ea4cd0ffb84566c8
강효민 <miamiq0000@gmail.com>
2023-08-25 23:03:34 +0900

    x

--- notes/note2_metaball/main.js
+++ notes/note2_metaball/main.js
@@ -1,4 +1,6 @@
 import thresholdFilter from "./thresholdFilter.js";
+
+
 class App {
 
     constructor() {
@@ -45,12 +47,8 @@ class App {
         return circle
     }
     setObj(){
-        // const canvas = new PIXI.Graphics()
-        // this.canvas = canvas
-        // canvas.beginFill(0, 0);
-        // canvas.lineStyle(0);
-        // canvas.drawRect(0, 0, window.innerWidth, window.innerHeight);
         const canvas = new PIXI.Container()
+        this.canvas = canvas
         this.app.stage.addChild(canvas);
         canvas.filterArea = new PIXI.Rectangle(0,0,innerWidth, innerHeight)
 
@@ -58,21 +56,20 @@ class App {
         canvas.height = innerHeight
         
         this.app.stage.addChild(canvas)
-        canvas.addChild(this.circle(innerWidth / 2,innerHeight / 2,100))
-        const movingCircle = this.circle(innerWidth / 2,0,100)
-
-        canvas.addChild(movingCircle)
-        this.movingCircle = movingCircle
         
         canvas.filters = [
             new PIXI.BlurFilter(10,undefined, undefined,15),
             thresholdFilter(0.95),
-            // new PIXI.BlurFilter(2,undefined, undefined,)
             new PIXI.FXAAFilter()
         ]
         const circleNum = 10;
         for(let i = 0; i<circleNum; i++){
-            canvas.addChild(this.circle(innerWidth * Math.random(), innerHeight * Math.random(),100 * Math.random()))
+            const circle = this.circle(innerWidth * Math.random(), innerHeight * Math.random(),100 * Math.random())
+            circle.x = Math.random() * innerWidth;
+            circle.y = Math.random() * innerHeight;
+            circle.vx = Math.random() * 3;
+            circle.vy = Math.random() * 3;
+            canvas.addChild(circle)
         }
 
 
@@ -107,9 +104,32 @@ class App {
         
     }
     setTicker() {
+        
+        // noise.seed(Math.random());
+        let time = 1;
+        
+        const moveCircles = (delta)=>{
+            time += delta;
+            
+            for(let circle of this.canvas.children){
+                circle.x += circle.vx
+                circle.y += circle.vy
+                if(circle.x < 0){
+                    circle.vx *= -1;
+                }else if(circle.x > innerWidth){
+                    circle.vx *= -1;
+                }
+                if(circle.y < 0){
+                    circle.vy *= -1;
+                }else if(circle.y > innerWidth){
+                    circle.vy *= -1;
+                }
+
+            }
+        }
         this.app.ticker.add(delta => {
-            // this.app.renderer.render(this.app.stage)
-            this.movingCircle.y += delta
+            moveCircles(delta)
+
         })
     }
 
