fdef7690ef5d1153dcdec7db4b856874bc655a20
hyomin <miamiq0000@gmail.com>
2024-02-10 17:38:24 +0900

    x

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 BZ Pattern : https://singrum.github.io/pixijsnote/notes/note1_BZ/BZ  
 Metaball : https://singrum.github.io/pixijsnote/notes/note2_metaball/metaball  
 Equipotential Metaball : https://singrum.github.io/pixijsnote/notes/note5_equipotential/index  
-Rock Scissors Paper : https://singrum.github.io/pixijsnote/notes/note6_rock_scissors_paper
+Rock Scissors Paper : https://singrum.github.io/pixijsnote/notes/note6_rock_scissors_paper  
+RSP with physics : https://singrum.github.io/pixijsnote/notes/note7_RSP_physics  
---
+++ notes/note7_RSP_physics/main.js
@@ -0,0 +1,156 @@
+
+const getRandom = (min, max) => {
+    if (max === undefined) {
+        return Math.random() * min
+    }
+    return Math.random() * (max - min) + min
+
+}
+
+const getWinner = (p1, p2) => {
+    
+    if ((p1 + 1) % 3 === p2)
+        return 1 //p2승
+    else if (p1 === p2)
+        return -1
+    else
+        return 0 //p1승
+}
+Array.prototype.pairs = function (func) {
+    for (let i = 0; i < this.length - 1; i++) {
+        for (let j = i; j < this.length - 1; j++) {
+            func([this[i], this[j + 1]]);
+        }
+    }
+}
+class App {
+
+    constructor() {
+
+        this.startGame()
+
+
+    }
+    startGame() {
+
+
+        // create an engine
+        let engine = Matter.Engine.create();
+        this.engine = engine
+        // create a renderer
+        let render = Matter.Render.create({
+            element: document.body,
+            engine: engine,
+            options: {
+                width: innerWidth,
+                height: innerHeight,
+                wireframes: false,
+                showAngleIndicator: false,
+                background: 0xffffff
+            }
+        });
+
+        // create two boxes and a ground
+
+        let walls = [
+            Matter.Bodies.rectangle(innerWidth / 2, innerHeight + 100, innerWidth, 200, { isStatic: true }), // bottom
+            Matter.Bodies.rectangle(innerWidth / 2, -100, innerWidth, 200, { isStatic: true }), // top
+            Matter.Bodies.rectangle(-100, innerHeight / 2, 200, innerHeight, { isStatic: true }), // left
+            Matter.Bodies.rectangle(innerWidth + 100, innerHeight / 2, 200, innerHeight, { isStatic: true })
+        ]
+
+
+        const objInfo = {
+            "0": { num: 30, type: "rock" },
+            "1": { num: 30, type: "paper" },
+            "2": { num: 30, type: "scissors" }
+        }
+
+
+        const objs = []
+        this.objs = objs
+        for (const [name, value] of Object.entries(objInfo)) {
+            for (let i = 0; i < value.num; i++) {
+                const obj = Matter.Bodies.circle(getRandom(innerWidth - 32), getRandom(innerHeight - 32), 16,
+                    {
+                        render: {
+                            sprite: {
+                                texture: `./asset/${value.type}.png`,
+                                xScale: 0.2,
+                                yScale: 0.2,
+                            }
+                        }
+                    })
+                
+                obj.name = parseInt(name)
+
+                objs.push(obj)
+            }
+        }
+
+
+        // add all of the bodies to the world
+        Matter.Composite.add(engine.world, [...objs, ...walls]);
+
+        // run the renderer
+        Matter.Render.run(render);
+
+        // create runner
+        let runner = Matter.Runner.create();
+        this.runner = runner
+        // run the engine
+        Matter.Runner.run(runner, engine);
+        this.setEvent()
+        this.setOrientationControl()
+    }
+    setEvent() {
+        
+        const onCollision = ()=>{
+            this.objs.pairs(pair => {
+                const collide = Matter.Collision.collides(pair[0], pair[1])
+                if (!collide) {
+                    return
+                }
+                const winner = getWinner(pair[0].name, pair[1].name)
+                
+                if(winner === -1){
+                    return
+                }
+                
+
+                pair[1 - winner].render.sprite.texture = pair[winner].render.sprite.texture
+                pair[1 - winner].name = pair[winner].name
+                
+            });
+        }
+        Matter.Events.on(this.runner, 'afterTick', function () {
+            onCollision()
+        })
+    }
+    setOrientationControl(){
+        
+        window.addEventListener('deviceorientation', evt=>{
+        
+            if( ! (evt.alpha && evt.beta && evt.gamma)){
+                return;
+            }
+            const alpha = evt.alpha * Math.PI / 180;
+            const beta = evt.beta * Math.PI / 180;
+            const gamma = evt.gamma * Math.PI / 180;
+            
+            const k = 9
+            const gravityX = Math.cos(beta) * Math.sin(gamma) * k
+            const gravityY = -Math.sin(beta) * k
+            const gravityZ = -Math.cos(beta) * Math.cos(gamma) * 9
+            this.engine.x = gravityX
+            this.engine.y = gravityY
+        
+
+            
+        });
+    }
+
+
+}
+
+new App()
